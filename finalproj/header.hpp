f2c238ff2daf14428284f55699e109b10f2ab4a0
fatma M <fatma_tamtoma@yahoo.com>
2013-11-19 16:38:20 -0500

    header names and includes

---
+++ finalproj/header.hpp
@@ -0,0 +1,19 @@
+/*
+ * header.hpp
+ *
+ *  Created on: Nov 19, 2013
+ *      Author: ahmed
+ */
+
+#ifndef HEADER_HPP_
+#define HEADER_HPP_
+
+#include <iostream>
+#include <string>
+#include <time.h>
+#include <cstdlib>
+using namespace std;
+
+
+
+#endif /* HEADER_HPP_ */
